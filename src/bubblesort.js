6136574f39acb980c61aed6f9c12a215aaaace91
ZKaptan <dkkaptan20@gmail.com>
2021-11-07 09:05:04 -0400

    New version

---
+++ src/bubblesort.js
@@ -0,0 +1,56 @@
+export const bubblesort = async (speed) => {
+	const array = document.getElementsByClassName("bar");
+	const size = array.length;
+	let last = size - 1;
+	for (let i = 0; i < size - 1; i++) {
+		for (let j = 0; j < size - i - 1; j++) {
+			array[j].style.backgroundColor = "green";
+			array[j + 1].style.backgroundColor = "green";
+			if (
+				parseInt(array[j].style.height) > parseInt(array[j + 1].style.height)
+			) {
+				array[j].style.backgroundColor = "red";
+				array[j + 1].style.backgroundColor = "red";
+				await waitForme(speed);
+				swap(array[j], array[j + 1]);
+			}
+			array[j].style.backgroundColor = "orange";
+			array[j + 1].style.backgroundColor = "orange";
+		}
+		array[last--].style.backgroundColor = "blue";
+	}
+	array[last].style.backgroundColor = "blue";
+
+	return true;
+};
+
+const disableButtons = () => {
+	document.getElementById();
+};
+const waitForme = (speed) => {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve("");
+		}, speed);
+	});
+};
+
+const swap = (bar1, bar2) => {
+	let temp = bar1.style.height;
+	bar1.style.height = bar2.style.height;
+	bar2.style.height = temp;
+};
+
+export const bubbleSortTest = (array) => {
+	const arr1 = bubblesort(array);
+	const arr2 = array.sort();
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+	for (let i = 0; i < arr1.length; i++) {
+		if (arr1[i] !== arr2[i]) {
+			return false;
+		}
+	}
+	return true;
+};
--- src/components/ArrayBar.js
+++ src/components/ArrayBar.js
@@ -1,15 +1,18 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./arrayBar.css";
-const ArrayBar = () => {
+import { bubblesort } from "../bubblesort";
+
+const ArrayBar = (props) => {
 	const [arrayBars, setArrayBars] = useState([]);
+	const [disableButton, setDisableButton] = useState(false);
 
 	useEffect(() => {
 		generateArray();
 	}, []);
 
-	const NUMBER_OF_BARS = 150;
+	const NUMBER_OF_BARS = 50;
 
-	const generateArray = () => {
+	const generateArray = (e) => {
 		const tempArray = [];
 		for (let i = 0; i < NUMBER_OF_BARS; i++) {
 			tempArray.push(randomIntFromInterval(5, 750));
@@ -20,8 +23,48 @@ const ArrayBar = () => {
 		return Math.floor(Math.random() * (max - min + 1) + min);
 	};
 
+	const clickHandler = () => {
+		generateArray();
+		resetArray();
+	};
+
+	const resetArray = () => {
+		const bars = document.getElementsByClassName("bar");
+		for (let i = 0; i < bars.length; i++) {
+			bars[i].style.backgroundColor = "pink";
+		}
+	};
+
+	const disableButtons = () => {};
+
+	const sortClickHandler = () => {
+		const speedSlider = document.getElementById("speed_slider");
+		const speed = speedSlider.value;
+
+		setDisableButton(true);
+		bubblesort(speed).then(() => {
+			setDisableButton(false);
+		});
+	};
+
 	return (
 		<div className="container">
+			<header>
+				<button disabled={disableButton} onClick={clickHandler}>
+					New Array
+				</button>
+				<label htmlFor="speed_slider">Speed</label>
+				<input
+					type="range"
+					id="speed_slider"
+					min="5"
+					max="50"
+					defaultValue="20"
+				/>
+				<button disabled={disableButton} onClick={sortClickHandler}>
+					Bubble Sort
+				</button>
+			</header>
 			<div className="array-container">
 				{arrayBars.map((value, index) => {
 					return (
@@ -33,7 +76,6 @@ const ArrayBar = () => {
 					);
 				})}
 			</div>
-			<button onClick={generateArray}>Generate New Array</button>
 		</div>
 	);
 };
